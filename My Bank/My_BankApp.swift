8b562d0d9e8220f52be1cdfbdd36bf8b24a781c9
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-09-07 16:27:30 -0400

    Initial Commit

---
+++ My Bank/My_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_BankApp.swift
+//  My Bank
+//
+//  Created by Gustavo Garfias on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct My_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
