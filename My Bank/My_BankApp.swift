d6f30f50951b32a67b9ba108700bc2bc86173cf0
Gustavo Garfias <gustavo.egarfias@gmail.com>
2025-09-07 17:35:17 -0400

    remove unused app delegate adaptor

--- My Bank/My_BankApp.swift
+++ My Bank/My_BankApp.swift
@@ -9,18 +9,12 @@ import SwiftUI
 import FirebaseCore
 import FirebaseAuth
 
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//    func application(_ application: UIApplication,
-//                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//        FirebaseApp.configure()
-//        return true
-//    }
-//}
 
 @main
 struct My_BankApp: App {
 //    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @State var userVM: UserViewModel
+    @State var accountsVM: AccountsViewModel = .init()
     
     init() {
         FirebaseApp.configure()
